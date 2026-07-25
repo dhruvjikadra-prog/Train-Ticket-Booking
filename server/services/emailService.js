53672181d8b930a8feaa865336304a46b3d01f9c
dhruvjikadra-prog <dhruvjikadra@gmail.com>
2026-07-25 12:09:33 +0530

    Email Sending Bug Fix

--- server/services/emailService.js
+++ server/services/emailService.js
@@ -6,20 +6,12 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 587,
-    secure: false, // IMPORTANT
-
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
-
-    requireTLS: true,
-
-    connectionTimeout: 30000,
-    greetingTimeout: 30000,
-    socketTimeout: 30000,
-
     logger: true,
     debug: true,
 });
